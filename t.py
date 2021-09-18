c164e3e673c741499956fbc65d040626bb2f8d0e
Jack.ly <811785237@qq.com>
2021-09-18 09:56:40 +0800

    Create t.py
    for test

---
+++ t.py
@@ -0,0 +1,14 @@
+    for r in regi_list:
+        # r = 'cn-guangzhou'
+        print('region:',r)
+        # print('cn-hangzhou')
+        clt = client.AcsClient('LTAI5tLdUMxQAUNeSAGFL8Ho', 'pG2g4K19skdia7YDuyg4Hi4lrYyXtE', r)
+
+        try:
+            list_instances()
+        except Exception as e:
+            # print(e)
+            pass
+
+        time.sleep(3)
+        print('###################################################################################\n\n')
